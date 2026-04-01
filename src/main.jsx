d0359c450d31f30709c23de9e6ddd3d833289445
Liliia Zuziak <liliazuziak724@gmail.com>
2026-04-02 02:43:45 +0300

    Add Sentry monitoring for frontend (React)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,23 @@
+import * as Sentry from "@sentry/react";
 import { RoomContext } from './context/RoomContext';
 import ReactDOM from 'react-dom/client'
 import React from 'react'
 import App from './App'
 import './style/index.css';
+
+Sentry.init({
+  dsn: "https://baeecf0bd9f921e18a19a2683db46b5a@o4511147327029248.ingest.de.sentry.io/4511147414454352",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/api\.runabooking\.me/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  sendDefaultPii: true,
+  enableLogs: true,
+});
 
 
 ReactDOM
